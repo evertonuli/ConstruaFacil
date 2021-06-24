8202aa7a6ed7191d26fe37dcb1dd1ca812cad4b7
Evrton Ulisses <evertonuli@gmail.com>
2021-06-24 15:43:21 -0300

    Initial commit

---
+++ src/main/java/br/com/gotasdetecnologia/devcalc/Calc.java
@@ -0,0 +1,17 @@
+//1-pacote
+package br.com.gotasdetecnologia.devcalc;
+
+//bibliotecas
+
+//classe
+public class Calc {
+
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo. Passou pelo metodo main");
+        somarDoisNumeros();
+        //
+    }
+    public static void somarDoisNumeros(){
+        System.out.println("Passou pelo método somarDoisNumeros ");
+    }
+}
